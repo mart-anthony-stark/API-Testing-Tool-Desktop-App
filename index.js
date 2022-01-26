7eae18d4569b0e619cf028b3b2d1f2aa054ae22d
mart-anthony-stark <martsalazar0@gmail.com>
2022-01-26 20:38:36 +0800

    added app init

---
+++ index.js
@@ -0,0 +1,75 @@
+const electron = require("electron");
+const url = require("url");
+const path = require("path");
+
+const { app, BrowserWindow, Menu } = electron;
+
+let mainWindow;
+
+app.on("ready", () => {
+  // Create main window
+  mainWindow = new BrowserWindow({
+    width: 1000,
+    height: 800,
+    title: "API Testing Tool",
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  //   Load html to window
+  mainWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "views/mainWindow.html"),
+      protocol: "file:",
+      slashes: true,
+    })
+  );
+
+  //   Close app when main window is closed
+  mainWindow.on("closed", () => {
+    app.quit();
+  });
+
+  //   Build menu from template
+  const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+  // Insert menu
+  Menu.setApplicationMenu(mainMenu);
+});
+
+// Create menu template
+const mainMenuTemplate = [
+  {
+    label: "Apllication",
+    submenu: [
+      {
+        label: "Quit",
+        click: () => {
+          app.quit();
+        },
+        accelerator: process.platform === "darwin" ? "Command+Q" : "Ctrl+Q",
+      },
+    ],
+  },
+];
+
+if (process.platform === "darwin") mainMenuTemplate.unshift({});
+// Add dev tools if not in prod
+if (process.env.NODE_ENV !== "production") {
+  mainMenuTemplate.push({
+    label: "Developer Tools",
+    submenu: [
+      {
+        role: "reload",
+      },
+      {
+        label: "Toggle Dev Tools",
+        click(item, focusedWindow) {
+          focusedWindow.toggleDevTools();
+        },
+        accelerator: process.platform === "darwin" ? "Command+i" : "Ctrl+i",
+      },
+    ],
+  });
+}
