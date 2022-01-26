e64ebb3a26b996dcdcafacdd0d8ac8d79e1cede7
mart-anthony-stark <martsalazar0@gmail.com>
2022-01-26 21:58:14 +0800

    added event listener

--- views/app.js
+++ views/app.js
@@ -1,39 +1,50 @@
 const historyListCont = document.querySelector("#history-list");
 const urlInput = document.querySelector("#url");
 const sendBtn = document.querySelector("#sendBtn");
-const history = localStorage.getItem("history");
+const bodyInput = document.querySelector(
+  "body > main > div.request-container > div > textarea"
+);
 
 const loader = document.querySelector(
   "body > main > div.response-container > div.loader"
 );
 
+const history = localStorage.getItem("history");
 if (!history || history.length == 0) {
-  historyListCont.innerHTML = "<h3>No request made yet</h3>";
+  historyListCont.innerHTML = "<p>No request made yet</p>";
 }
 
-const getData = async (
-  method,
-  url,
-  body = {},
-  headers = {
-    "Content-Type": "application/json",
-  }
-) => {
-  loader.style.display = "block";
-
-  const res = await fetch(url, {
-    method,
-    headers,
-    body: JSON.stringify(body),
-  });
-  const data = await res.json();
-  console.log(data);
-  loader.style.display = "none";
-};
-
 sendBtn.addEventListener("click", (e) => {
+  if (urlInput.value.length === 0) {
+    console.log("Invalid url");
+    return;
+  }
+
   const req = {
     url: urlInput.value,
     method: "get",
+    body: bodyInput.value,
   };
+  req.body = req.body.replaceAll(/\\"/g, '"');
+  console.log(req);
 });
+
+// const getData = async (
+//   method,
+//   url,
+//   body = {},
+//   headers = {
+//     "Content-Type": "application/json",
+//   }
+// ) => {
+//   loader.style.display = "block";
+
+//   const res = await fetch(url, {
+//     method,
+//     headers,
+//     body: JSON.stringify(body),
+//   });
+//   const data = await res.json();
+//   console.log(data);
+//   loader.style.display = "none";
+// };
