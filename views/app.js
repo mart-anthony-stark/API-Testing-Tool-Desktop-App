cc4195ca6bb15567c7c3c012bb89da91a020cf5d
mart-anthony-stark <martsalazar0@gmail.com>
2022-02-02 19:07:34 +0800

    added helper methods

--- views/app.js
+++ views/app.js
@@ -1,3 +1,4 @@
+const { validateUrl } = require("./helper");
 const historyListCont = document.querySelector("#history-list");
 const urlInput = document.querySelector("#url");
 const sendBtn = document.querySelector("#sendBtn");
@@ -16,19 +17,12 @@ if (!history || history.length == 0) {
   historyListCont.innerHTML = "<p>No request made yet</p>";
 }
 
-function validateUrl(value) {
-  // return /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?$/i.test(
-  //   value
-  // );
-  return true;
-}
-
 const sendRequest = (e) => {
   const method = document.querySelector("#request-method").value.toLowerCase();
   let isValid = validateUrl(urlInput.value);
-  if (urlInput.value.length === 0) {
-    isValid = false;
-  }
+  // if (urlInput.value.length === 0) {
+  //   isValid = false;
+  // }
 
   if (!isValid) {
     console.log("Invalid url");
