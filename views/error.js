99f0a7020cc267f93521c896543039501c34b59d
mart-anthony-stark <martsalazar0@gmail.com>
2022-02-02 19:29:46 +0800

    added error script module

---
+++ views/error.js
@@ -0,0 +1,8 @@
+const errorOverlay = document.querySelector(".error-overlay");
+const errorTxt = document.querySelector("p.error");
+const okayBtn = document.querySelector("#close-error");
+const { hideEl } = require("./helper");
+
+okayBtn.addEventListener("click", () => {
+  hideEl(errorOverlay);
+});
